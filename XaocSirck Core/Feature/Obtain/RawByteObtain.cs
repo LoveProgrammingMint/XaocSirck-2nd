360c0286e46ef40f32ab78f3d2d8f1eb4ed38d8e
LoveProgrammingMint <mint121986@126.com>
2026-07-08 22:10:54 +0800

    New feature: Byte obtain

---
+++ XaocSirck Core/Feature/Obtain/RawByteObtain.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+using XaocSirck_Core.Interface.Feature;
+
+namespace XaocSirck_Core.Feature.Obtain;
+
+internal sealed unsafe class RawByteObtain : IFeatureObtain
+{
+    private readonly Int32 _bufferSize = 16388;
+    private Byte* _resultPtr = (Byte*)NativeMemory.AlignedAlloc((UIntPtr)16388, (UIntPtr)32);
+    private String _inputData = String.Empty;
+    private Boolean _disposed;
+
+    internal RawByteObtain()
+    {
+        Clear();
+    }
+
+    public void Clear()
+    {
+        ObjectDisposedException.ThrowIf(_disposed, nameof(RawByteObtain));
+        Span<Byte> result = new(_resultPtr, _bufferSize);
+        result.Clear();
+    }
+
+    public void Dispose()
+    {
+        if (!_disposed)
+        {
+            if (_resultPtr != null) { NativeMemory.AlignedFree(_resultPtr); }
+            _resultPtr = null;
+            _disposed = true;
+            GC.SuppressFinalize(this);
+        }
+    }
+
+    public IntPtr GetResult()
+    {
+        ObjectDisposedException.ThrowIf(_disposed, nameof(RawByteObtain));
+        return (IntPtr)_resultPtr;
+    }
+
+    public void Obtain()
+    {
+        ObjectDisposedException.ThrowIf(_disposed, nameof(RawByteObtain));
+        using FileStream stream = File.OpenRead(_inputData);
+        Int32 needsToRead = (Int32)Math.Min(_bufferSize, stream.Length);
+        Span<Byte> span = new(_resultPtr, needsToRead);
+        stream.ReadExactly(span);
+    }
+
+    public void Set(Object inputData)
+    {
+        ObjectDisposedException.ThrowIf(_disposed, nameof(RawByteObtain));
+        if (inputData is not String path)
+        {
+            throw new ArgumentException("Input data must be a string representing the file path.", nameof(inputData));
+        }
+        _inputData = path;
+    }
+}
---
+++ XaocSirck Core/Interface/Feature/IFeatureObtain.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XaocSirck_Core.Interface.Feature;
+
+internal interface IFeatureObtain : IDisposable
+{
+    public void Obtain();
+    public IntPtr GetResult();
+    public void Set(Object inputData);
+    public void Clear();
+}
