eec83d4b3bfc37db28dbbf0c61fe6f689e759041
LoveProgrammingMint <mint121986@126.com>
2026-07-08 22:11:13 +0800

    New feature: Setting system

---
+++ XaocSirck Core/App.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using XaocSirck_Core.Engine;
+
+namespace XaocSirck_Core;
+
+internal static class App
+{
+    public static Settings Settings { get; } = new();
+}
---
+++ XaocSirck Core/Engine/Settings.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace XaocSirck_Core.Engine;
+
+// Json Model Classes
+internal class Root { }
+
+[JsonSerializable(typeof(Root))]
+public partial class AppJsonContext : JsonSerializerContext { }
+
+
+
+internal class Settings
+{
+    public Root? Config = JsonSerializer.Deserialize(File.ReadAllText("configs.json"), AppJsonContext.Default.Root);
+
+    public void ReLoad()
+    {
+        Config = JsonSerializer.Deserialize(File.ReadAllText("configs.json"), AppJsonContext.Default.Root);
+    }
+
+    public void Save()
+    {
+        string jsonString = JsonSerializer.Serialize(Config, AppJsonContext.Default.Root);
+        File.WriteAllText("configs.json", jsonString);
+    }
+}
