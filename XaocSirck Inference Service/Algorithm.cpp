479b8cace72a0874dc08022096511c1274051b5a
LoveProgrammingMint <mint121986@126.com>
2026-07-09 22:00:16 +0800

    New feature: Algorithm, added softmax

---
+++ XaocSirck Inference Service/Algorithm.cpp
@@ -0,0 +1,30 @@
+#include "Algorithm.h"
+#include <algorithm>
+#include <cmath>
+
+std::vector<Single> Algorithm::Softmax(const std::vector<Single>& input)
+{
+    if (input.empty())
+    {
+        return {};
+    }
+
+    Single maxValue = *std::max_element(input.begin(), input.end());
+    std::vector<Single> output(input.size());
+    Single sum = 0.0f;
+
+    for (size_t i = 0; i < input.size(); ++i)
+    {
+        output[i] = std::exp(input[i] - maxValue);
+        sum += output[i];
+    }
+
+    Single inverseSum = 1.0f / sum;
+
+    for (size_t i = 0; i < input.size(); ++i)
+    {
+        output[i] *= inverseSum;
+    }
+
+    return output;
+}
---
+++ XaocSirck Inference Service/Algorithm.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "Base.h"
+#include <vector>
+
+class Algorithm
+{
+public:
+    static std::vector<Single> Softmax(const std::vector<Single>& input);
+};
