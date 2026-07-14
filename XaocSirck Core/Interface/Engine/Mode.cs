cd6db5fffce826334d08800f40402eba5d93a014
LoveProgrammingMint <mint121986@126.com>
2026-07-14 12:19:22 +0800

    New feature: Modes

--- XaocSirck Core/Interface/Engine/Mode.cs
+++ XaocSirck Core/Interface/Engine/Mode.cs
@@ -6,6 +6,7 @@ namespace XaocSirck_Core.Interface.Engine;
 
 internal enum _Mode_Bitremal : Byte
 {
+    Disabled,
     Rb,
     Al,
     Ot
@@ -13,16 +14,57 @@ internal enum _Mode_Bitremal : Byte
 
 internal enum _Mode_Zeroflows : Byte
 {
+    Disabled,
     Zf
 }
 
 internal enum _Mode_Signature : Byte
 {
+    Disabled,
     Loose,
     Strict
 }
 
-internal enum _
+internal enum _Mode_Documentation : Byte
 {
-    
+    Disabled,
+    DocVBA
+}
+
+internal enum _Mode_Shell : Byte
+{
+    Disabled,
+    Block,
+    Suspicious
+}
+
+internal enum _Mode_Archive : Byte
+{
+    Disabled,
+    Check
+}
+
+internal struct _Mode_Engines
+{
+    public _Mode_Signature Signature;
+    public _Mode_Archive Archive;
+    public _Mode_Documentation Documentation;
+    public _Mode_Shell Shell;
+}
+
+internal enum _Mode_Charwolf : Byte
+{
+    Disabled,
+    Core,
+    Extended,
+    Fulled
+}
+
+internal struct EngineMode
+{
+    public _Mode_Bitremal Bitremal;
+    public _Mode_Zeroflows Zeroflow;
+    public _Mode_Signature Signature;
+    public _Mode_Charwolf Charwolf;
+    public _Mode_Engines Engines;
 }
